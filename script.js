d7872efd6af92aab6e7033b56dbdc2764e1ad205
Cambier-Adrien <141322669+Cambier-Adrien@users.noreply.github.com>
2023-08-14 21:20:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,87 @@
-function toggleLanguage(){
+function toggleLanguage() {
   const languageSelector = document.querySelector('.language-selector');
   const languageContent = document.querySelector('.language-content');
 
-  languageContent.addEventListener('click',function() {
+  languageContent.addEventListener('click', function() {
     languageSelector.classList.toggle('visible');
   });
-  
+
   const fr = document.querySelector('#fr');
   const en = document.querySelector('#en');
 
-  fr.addEventListener('click',function() {
-    languageSelector.classList.toggle('visible');
+  fr.addEventListener('click', function() {
     const metaLanguage = document.querySelector('meta[http-equiv="Content-Language"]');
     if (metaLanguage && metaLanguage.getAttribute('content') === 'fr') {
+      languageSelector.classList.remove('visible'); // Ne cache le sélecteur que si la langue est déjà fr
       return;
     } else {
-    window.location.href="index.html";}
+      window.location.href = "index.html";
+    }
   });
-  en.addEventListener('click',function() {
-    languageSelector.classList.toggle('visible');
+
+  en.addEventListener('click', function() {
     const metaLanguage = document.querySelector('meta[http-equiv="Content-Language"]');
     if (metaLanguage && metaLanguage.getAttribute('content') === 'en') {
+      languageSelector.classList.remove('visible'); // Ne cache le sélecteur que si la langue est déjà en
       return;
     } else {
-    window.location.href="index-en.html";}
+      window.location.href = "index-en.html";
+    }
   });
 }
 
 toggleLanguage();
 
-function toggleMenu(){
+function toggleMenu() {
   const hamburgerMenu = document.querySelector('.hamburger-menu');
   const backgroundNav = document.querySelector('.background-nav');
   const Nav = document.querySelector('.nav-links');
   const close = document.querySelector('.close');
   const links = document.querySelectorAll('.nav-links .links');
   const nameMenuText = document.querySelector('.name-menu-text');
-  
+
   hamburgerMenu.addEventListener('click', function() {
     backgroundNav.classList.remove('hidden');
     Nav.classList.add('menu-on');
   });
-  close.addEventListener('click', function(){
+  close.addEventListener('click', function() {
     backgroundNav.classList.add('hidden');
     Nav.classList.remove('menu-on');
   });
-  nameMenuText.addEventListener('click', function(){
+  nameMenuText.addEventListener('click', function() {
     backgroundNav.classList.add('hidden');
     Nav.classList.remove('menu-on');
   });
-  links.forEach(link=>{
-    link.addEventListener('click',function(){
+  links.forEach(link => {
+    link.addEventListener('click', function() {
       backgroundNav.classList.add('hidden');
       Nav.classList.remove('menu-on');
-      });
+    });
   });
 }
-  
+
 toggleMenu();
 
-function addSmoothScrollToAnchors(){
+function addSmoothScrollToAnchors() {
   const anchors = document.querySelectorAll('a[href^="#"]');
-  
+
   anchors.forEach(anchor => {
     anchor.addEventListener('click', function(event) {
-      event.preventDefault(); // Empêche le comportement de clic par défaut
-  
-      const targetId = anchor.getAttribute('href'); // Obtient la valeur de l'attribut href
-      const targetElement = document.querySelector(targetId); // Sélectionne l'élément cible
-  
+      event.preventDefault();
+
+      const targetId = anchor.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
       if (targetElement) {
-        const offsetTop = targetElement.getBoundingClientRect().top + window.pageYOffset; // Calcul du décalage
-  
+        const offsetTop = targetElement.getBoundingClientRect().top + window.pageYOffset;
+
         window.scrollTo({
           top: offsetTop,
-          behavior: 'smooth' // Utilise l'animation fluide
+          behavior: 'smooth'
         });
       }
     });
   });
 }
-  
+
 addSmoothScrollToAnchors();
