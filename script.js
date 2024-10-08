c96c885c04b41a86dd5302e60ddd3c95349e4d35
Cambier-Adrien <141322669+Cambier-Adrien@users.noreply.github.com>
2024-10-08 12:16:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    
+    const target = document.querySelector(this.getAttribute("href"));
+
     if (target) {
       window.scrollTo({
-        top: target.offsetTop - 3.5 * parseFloat(getComputedStyle(document.documentElement).fontSize), 
-        behavior: 'smooth'   
+        top:
+          target.offsetTop -
+          3.5 * parseFloat(getComputedStyle(document.documentElement).fontSize),
+        behavior: "smooth",
       });
     }
   });
@@ -17,33 +19,37 @@ toggleLanguage();
 searchPortfolio();
 
 function toggleLanguage() {
-  const languageSelector = document.querySelector('.language-selector');
-  const languageContent = document.querySelector('.language-content');
+  const languageSelector = document.querySelector(".language-selector");
+  const languageContent = document.querySelector(".language-content");
 
-  languageContent.addEventListener('click', function() {
-    languageSelector.classList.toggle('visible');
+  languageContent.addEventListener("click", function () {
+    languageSelector.classList.toggle("visible");
   });
 
-  const fr = document.querySelector('#fr');
-  const en = document.querySelector('#en');
+  const fr = document.querySelector("#fr");
+  const en = document.querySelector("#en");
 
-  fr.addEventListener('click', function() {
-    const metaLanguage = document.querySelector('meta[http-equiv="Content-Language"]');
-    if (metaLanguage && metaLanguage.getAttribute('content') === 'fr') {
-        languageSelector.classList.remove('visible');
-        return;
+  fr.addEventListener("click", function () {
+    const metaLanguage = document.querySelector(
+      'meta[http-equiv="Content-Language"]'
+    );
+    if (metaLanguage && metaLanguage.getAttribute("content") === "fr") {
+      languageSelector.classList.remove("visible");
+      return;
     } else {
-        window.location.href = 'index.html';
+      window.location.href = "index.html";
     }
   });
 
-  en.addEventListener('click', function() {
-    const metaLanguage = document.querySelector('meta[http-equiv="Content-Language"]');
-    if (metaLanguage && metaLanguage.getAttribute('content') === 'en') {
-        languageSelector.classList.remove('visible');
-        return;
+  en.addEventListener("click", function () {
+    const metaLanguage = document.querySelector(
+      'meta[http-equiv="Content-Language"]'
+    );
+    if (metaLanguage && metaLanguage.getAttribute("content") === "en") {
+      languageSelector.classList.remove("visible");
+      return;
     } else {
-        window.location.href = `unavailable.html`;
+      window.location.href = `unavailable.html`;
     }
   });
 }
@@ -56,10 +62,14 @@ function searchPortfolio() {
   const projectBox = document.querySelectorAll(".project-box");
   const themeText = document.querySelector("#project-theme div p");
   const yearText = document.querySelector("#project-year div p");
-  const imgYearArrow = document.querySelector("#project-year div img[src='img/vers-le-bas.png']");
-  const imgThemeArrow = document.querySelector("#project-theme div img[src='img/vers-le-bas.png']");
+  const imgYearArrow = document.querySelector(
+    "#project-year div img[src='img/vers-le-bas.png']"
+  );
+  const imgThemeArrow = document.querySelector(
+    "#project-theme div img[src='img/vers-le-bas.png']"
+  );
 
-  themeSelect.addEventListener('click', function() {
+  themeSelect.addEventListener("click", function () {
     const tAll = document.querySelector("#tAll");
     const info = document.querySelector("#info");
     const network = document.querySelector("#network");
@@ -73,124 +83,119 @@ function searchPortfolio() {
     year.classList.remove("visible");
     imgYearArrow.classList.add("rotate90deg");
 
-    info.addEventListener('click', function() {
+    info.addEventListener("click", function () {
       imgInfo.classList.remove("invisible");
       imgNetwork.classList.add("invisible");
       imgCyber.classList.add("invisible");
       imgtAll.classList.add("invisible");
-      themeText.textContent ="Informatique";
-      projectBox.forEach(box => {
-        if (box.contains(box.querySelector('.info'))) {
+      themeText.textContent = "Informatique";
+      projectBox.forEach((box) => {
+        if (box.contains(box.querySelector(".info"))) {
           box.style.display = "flex";
         } else {
           box.style.display = "none";
         }
       });
-
     });
 
-    network.addEventListener('click', function() {
+    network.addEventListener("click", function () {
       imgInfo.classList.add("invisible");
       imgNetwork.classList.remove("invisible");
       imgCyber.classList.add("invisible");
       imgtAll.classList.add("invisible");
-      themeText.textContent ="Réseaux";
-      projectBox.forEach(box => {
-        if (box.contains(box.querySelector('.network'))) {
+      themeText.textContent = "Réseaux";
+      projectBox.forEach((box) => {
+        if (box.contains(box.querySelector(".network"))) {
           box.style.display = "flex";
         } else {
           box.style.display = "none";
         }
       });
-
     });
 
-    cyber.addEventListener('click', function() {
+    cyber.addEventListener("click", function () {
       imgInfo.classList.add("invisible");
       imgNetwork.classList.add("invisible");
       imgCyber.classList.remove("invisible");
       imgtAll.classList.add("invisible");
-      themeText.textContent ="Cybersécurité";
-      projectBox.forEach(box => {
-        if (box.contains(box.querySelector('.cyber'))) {
+      themeText.textContent = "Cybersécurité";
+      projectBox.forEach((box) => {
+        if (box.contains(box.querySelector(".cyber"))) {
           box.style.display = "flex";
         } else {
           box.style.display = "none";
         }
       });
-
     });
 
-    tAll.addEventListener('click', function() {
+    tAll.addEventListener("click", function () {
       imgInfo.classList.add("invisible");
       imgNetwork.classList.add("invisible");
       imgCyber.classList.add("invisible");
       imgtAll.classList.remove("invisible");
-      themeText.textContent ="Tout";
-      projectBox.forEach(box => {
-          box.style.display = "flex";
+      themeText.textContent = "Tout";
+      projectBox.forEach((box) => {
+        box.style.display = "flex";
       });
     });
-
   });
 
-  yearSelect.addEventListener('click', function() {
+  yearSelect.addEventListener("click", function () {
     const yAll = document.querySelector("#yAll");
     const y2023 = document.querySelector("#y2023");
     const y2024 = document.querySelector("#y2024");
     const c2023 = document.querySelector("#c2023");
     const c2024 = document.querySelector("#c2024");
-    const img2023 = document.querySelector('#y2023 img');
-    const img2024 = document.querySelector('#y2024 img');
+    const img2023 = document.querySelector("#y2023 img");
+    const img2024 = document.querySelector("#y2024 img");
     const imgyAll = document.querySelector("#yAll img");
     year.classList.toggle("visible");
     imgYearArrow.classList.toggle("rotate90deg");
     theme.classList.remove("visible");
     imgThemeArrow.classList.add("rotate90deg");
 
-    y2023.addEventListener('click', function() {
+    y2023.addEventListener("click", function () {
       c2023.style.display = "flex";
       c2024.style.display = "none";
-      yearText.textContent ="2023";
+      yearText.textContent = "2023";
       img2023.classList.remove("invisible");
       img2024.classList.add("invisible");
       imgyAll.classList.add("invisible");
     });
 
-    y2024.addEventListener('click', function() {
+    y2024.addEventListener("click", function () {
       c2023.style.display = "none";
       c2024.style.display = "flex";
-      yearText.textContent ="2024";
+      yearText.textContent = "2024";
       img2024.classList.remove("invisible");
       img2023.classList.add("invisible");
       imgyAll.classList.add("invisible");
     });
 
-    yAll.addEventListener('click', function() {  
+    yAll.addEventListener("click", function () {
       c2023.style.display = "flex";
       c2024.style.display = "flex";
-      yearText.textContent ="Tout";
+      yearText.textContent = "Tout";
       img2024.classList.add("invisible");
       img2023.classList.add("invisible");
       imgyAll.classList.remove("invisible");
     });
-
   });
 }
 
 function toggleMenu() {
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
-  const backgroundNav = document.querySelector('.background-nav');
-  const Nav = document.querySelector('.nav-links');
-  const close = document.querySelector('.close');
-  const links = document.querySelectorAll('.nav-links .links');
-  const nameMenuText = document.querySelector('.name-menu-text');
-  const body = document.querySelector('body');
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const backgroundNav = document.querySelector(".background-nav");
+  const Nav = document.querySelector(".nav-links");
+  const close = document.querySelector(".close");
+  const links = document.querySelectorAll(".nav-links .links");
+  const nameMenuText = document.querySelector(".name-menu-text");
+  const html = document.querySelector("html");
 
   function closeMenu() {
-    backgroundNav.classList.add('hidden');
-      Nav.classList.remove('menu-on');
-      body.style.overflowY = "scroll";
+    backgroundNav.classList.add("hidden");
+    Nav.classList.remove("menu-on");
+    html.style.overflowY = "scroll";
   }
 
   function updateOverflow() {
@@ -199,27 +204,25 @@ function toggleMenu() {
     }
   }
 
-  hamburgerMenu.addEventListener('click', function() {
-    backgroundNav.classList.remove('hidden');
-    Nav.classList.add('menu-on');
-    body.style.overflowY = "hidden";
-  })
+  hamburgerMenu.addEventListener("click", function () {
+    backgroundNav.classList.remove("hidden");
+    Nav.classList.add("menu-on");
+    html.style.overflowY = "hidden";
+  });
 
-  close.addEventListener('click', function() {
+  close.addEventListener("click", function () {
     closeMenu();
-  })
+  });
 
-  nameMenuText.addEventListener('click', function() {
+  nameMenuText.addEventListener("click", function () {
     closeMenu();
-  })
+  });
 
-  links.forEach(link => {
-    link.addEventListener('click', function() {
+  links.forEach((link) => {
+    link.addEventListener("click", function () {
       closeMenu();
-    })
+    });
+  });
 
-  })
-
-  window.addEventListener('resize', updateOverflow);
-
+  window.addEventListener("resize", updateOverflow);
 }
